80658acdb5d6a7e1ce3f5ef6bcce0395d0c559a5
Luis Ramos <luis.ramos.qa@gmail.com>
2024-03-13 22:30:38 -0400

    Remocion de codigo no usado

--- src/test/java/ganadero/basetest/BaseTest.java
+++ src/test/java/ganadero/basetest/BaseTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.io.IOException;
 import java.time.Duration;
 
 public class BaseTest {
